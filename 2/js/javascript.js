ed35d658c3158854834191a6ab40b10c5886c335
suileyan <3220145931@qq.com>
2023-11-13 12:14:15 +0800

    1

---
+++ 2/js/javascript.js
@@ -0,0 +1,125 @@
+//隐藏子元素
+$(function () {
+  $(".AiTechnology-introduce").hide();
+});
+//隐藏span
+function AiTechnologyIntroduceBorderNone() {
+  $(".AiTechnology-introduce-border")
+    .children("span")
+    .eq(0)
+    .stop()
+    .animate({ width: "0%" }, 800);
+  $(".AiTechnology-introduce-border")
+    .children("span")
+    .eq(1)
+    .stop()
+    .animate({ width: "0%" }, 800);
+  $(".AiTechnology-introduce-border")
+    .children("span")
+    .eq(2)
+    .stop()
+    .animate({ height: "0%" }, 800);
+  $(".AiTechnology-introduce-border")
+    .children("span")
+    .eq(3)
+    .stop()
+    .animate({ height: "0%" }, 800);
+}
+//缓慢显示span
+function AiTechnologyIntroduceBorderBlock() {
+  $(".AiTechnology-introduce-border")
+    .children("span")
+    .eq(0)
+    .stop()
+    .animate({ width: "100%" }, 1000);
+  $(".AiTechnology-introduce-border")
+    .children("span")
+    .eq(1)
+    .stop()
+    .animate({ width: "100%" }, 1000);
+  $(".AiTechnology-introduce-border")
+    .children("span")
+    .eq(2)
+    .stop()
+    .animate({ height: "100%" }, 1000);
+  $(".AiTechnology-introduce-border")
+    .children("span")
+    .eq(3)
+    .stop()
+    .animate({ height: "100%" }, 1000);
+}
+$(".AiTechnology-img").hover(
+  function () {
+    $(this).children("p").css("color", "var(--text-gray-hover)");
+    var src = $(this).children("img").attr("src");
+    var index = src.lastIndexOf(".");
+    var suffix = src.substring(index);
+    var prefix = src.substring(0, index);
+    if (prefix.indexOf("-h") != -1) {
+      prefix = prefix.replace("-h", "");
+    } else {
+      prefix = prefix + "-h";
+    }
+    var src = prefix + suffix;
+    $(this).children("img").attr("src", src);
+    $(this).css(
+      "background",
+      "linear-gradient(160deg,rgb(16,52,117),rgb(33,67,193))"
+    );
+    $(".AiTechnology-img").click(function () {
+      $(".AiTechnology-introduce").stop().fadeOut(200);
+      $(this).children(".AiTechnology-introduce").stop().fadeIn(200);
+      $(this).find("span").eq(0).stop().animate({ width: "100%" }, 1000);
+      $(this).find("span").eq(1).stop().animate({ width: "100%" }, 1000);
+      $(this).find("span").eq(2).stop().animate({ height: "100%" }, 1000);
+      $(this).find("span").eq(3).stop().animate({ height: "100%" }, 1000);
+    });
+  },
+  function () {
+    $(this).find("span").eq(0).stop().animate({ width: "0%" }, 1000);
+    $(this).find("span").eq(1).stop().animate({ width: "0%" }, 1000);
+    $(this).find("span").eq(2).stop().animate({ height: "0%" }, 1000);
+    $(this).find("span").eq(3).stop().animate({ height: "0%" }, 1000);
+    $(this).children("p").css("color", "var(--text-gray)");
+    $(this).css(
+      "background",
+      "linear-gradient(160deg,rgba(16,52,117,.8),rgba(33,67,193,.8))"
+    );
+    var src = $(this).children("img").attr("src");
+    var index = src.lastIndexOf(".");
+    var suffix = src.substring(index);
+    var prefix = src.substring(0, index);
+    if (prefix.indexOf("-h") != -1) {
+      prefix = prefix.replace("-h", "");
+    } else {
+      prefix = prefix + "-h";
+    }
+    var src = prefix + suffix;
+    $(this).children("img").attr("src", src);
+  }
+);
+
+$(".practice-title>p>a").click(function () {
+  $(this).addClass("active").siblings().removeClass("active");
+});
+
+$(window).scroll(function () {
+  if ($(document).scrollTop() >= 100) {
+    $("header").css("background", "var(--header-bg-down)");
+  } else {
+    $("header").css("background", "var(--header-bg)");
+  }
+});
+
+$(".navbar-toggler").click(function () {
+  $(".navbar").toggleClass("act");
+});
+
+$(".ad").click(function () {
+  $(this).toggleClass("active").siblings().removeClass("active");
+  $(".at")
+    .eq($(this).index(".ad"))
+    .addClass("act")
+    .siblings()
+    .removeClass("act");
+});
