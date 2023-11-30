fc6730869ec07ad3cea344de3764776bb85fd128
Sulley-naer <1444736434@qq.com>
2023-11-30 19:30:36 +0800

    up

--- 5-1/js/javascript.js
+++ 5-1/js/javascript.js
@@ -6,10 +6,44 @@ $(".AiTechnology .navbar .nav .nav-item").on("click", function () {
     .siblings(".nav-item")
     .children("a")
     .removeClass("active");
+  let pageText = $(this).attr("page");
+  qh(pageText);
 });
-
-$(document).ready(function () {
+function pb() {
   let box = $(".AiTechnology .main .pictureBox>picture");
   let widthPercentage = 100 / box.length;
   box.css("width", widthPercentage + "%");
+}
+$(document).ready(function () {
+  pb();
+});
+
+function qh(pageText) {
+  if (pageText !== undefined) {
+    $(".AiTechnology .main").load("page-" + pageText + ".html", function () {
+      pc(),pb();
+    });
+    $(".programme").load("page-" + pageText + "-1.html", function () {
+      pc(),pb();
+    });
+  }
+}
+
+$("body").ready(function () {
+  let key = parseInt(sessionStorage.getItem("click"));
+
+  if (key !== undefined && key !== null && key !== NaN && key !== "") {
+    // alert(key);
+    $(".AiTechnology .navbar .nav .nav-item")
+      .eq(key)
+      .children("a")
+      .addClass("active")
+      .parent()
+      .siblings(".nav-item")
+      .children("a")
+      .removeClass("active");
+    qh(key + 1);
+  } else { 
+    qh(1);
+  }
 });
