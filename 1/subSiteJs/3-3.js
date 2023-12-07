0a464e7121b430e67570b74292ae6b7deaf73864
suileyan <3220145931@qq.com>
2023-12-07 10:07:49 +0800

    修补

--- 1/subSiteJs/3-3.js
+++ 1/subSiteJs/3-3.js
@@ -34,15 +34,22 @@ $(document).ready(function () {
 
   //变大
   $('.summary main > div .content div[class*="col-"]').mouseenter(function () {
-    $(this).attr("class", "col-md-6").css("align-items", "center");
-    $(this).siblings().attr("class", "col-md-3");
+    $(this).attr("class", "col-6").css("align-items", "center");
+    $(this).siblings().attr("class", "col-3");
+    if ($(window).width() < 800) {
+      $('.summary main > div .content div[class*="col-"] .msgbox').hide();
+    }
+    $(this).find(".msgbox").show();
   });
 
   $(".summary main > div .content").mouseleave(function () {
     $(this)
       .find('div[class*="col-"]')
-      .attr("class", "col-md-4")
+      .attr("class", "col-4")
       .css("align-items", "flex-start");
+    if ($(window).width() < 800) {
+      $('.summary main > div .content div[class*="col-"] .msgbox').hide();
+    }
   });
 
   if (sessionStorage.getItem("high") == 0) {
